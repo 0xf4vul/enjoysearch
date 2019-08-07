635f09baa677a8f4b79b5e15745839f10ae6c586
luckrill <luckrill@163.com>
2019-08-07 15:04:27 +0800

    update requirements.txt

--- converter.py
+++ converter.py
@@ -8,9 +8,9 @@ import html2text
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import ActionChains
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver import ActionChains
 # from phantomjs_bin import executable_path
 import time
 import pprint
