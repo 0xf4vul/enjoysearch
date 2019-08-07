f86452b9247e890896c74b87552e442f09ae7c51
luckrill <luckrill@163.com>
2019-08-07 11:29:54 +0800

    server num set to 4

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
-#gunicorn -w 8 -b 0.0.0.0:4000 service:app
-gunicorn -w 8 -b 0.0.0.0:4000 service:app --reload -t 500 -D --access-logfile /var/log/gunicorn.log
+#gunicorn -w 4 -b 0.0.0.0:4000 service:app
+gunicorn -w 4 -b 0.0.0.0:4000 service:app --reload -t 500 -D --access-logfile /var/log/gunicorn.log
