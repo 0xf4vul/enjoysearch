7416195a6a8c3577b6604c48075c4a0eb84c7d73
luckrill <luckrill@163.com>
2019-08-26 15:04:48 +0800

    add robots.txt

--- change_text.py
+++ change_text.py
@@ -34,6 +34,31 @@ def change_text_info():
         shutil.copyfile(html2, html1)
             #print(lines)
 def new_change_text_info():
+    # define day time
+    day_time = [
+    {"start":"00:00", "end":"05:00", "name":"day_text1"},
+    {"start":"05:01", "end":"06:00", "name":"day_text2"},
+    {"start":"06:01", "end":"08:30", "name":"day_text3"},
+    {"start":"08:31", "end":"09:30", "name":"day_text4"},
+    {"start":"09:10", "end":"11:30", "name":"day_text5"},
+    {"start":"11:31", "end":"13:00", "name":"day_text6"},
+    {"start":"13:01", "end":"14:00", "name":"day_text7"},
+    {"start":"14:01", "end":"17:50", "name":"day_text8"},
+    {"start":"17:51", "end":"20:00", "name":"day_text9"},
+    {"start":"20:01", "end":"22:00", "name":"day_text10"},
+    {"start":"22:01", "end":"23:00", "name":"day_text11"},
+    {"start":"23:01", "end":"24:00", "name":"day_text12"},
+    ]
+
+    a = "08:42"
+    b = "21:02"
+    for d in day_time:
+        if (a > d["start"]) and (a < d["end"]):
+            print(d)
+        if (b > d["start"]) and (b < d["end"]):
+            print(d)
+        pass
+
     path = os.path.dirname(os.path.realpath(__file__))
     in_name = path + "/static/textinfo.toml"
     with open(in_name, 'r', encoding='utf-8') as fd:
