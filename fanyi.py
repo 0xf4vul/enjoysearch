b1c666ee4ad44a4ffd3ce0013637e82eb06dfa3d
luckrill <luckrill@163.com>
2019-09-23 14:34:22 +0800

    google translate TK now use python code

--- fanyi.py
+++ fanyi.py
@@ -2,7 +2,9 @@
 import os
 import requests
 import time
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 from user_agents import random_user_agent
 from urllib.parse import urljoin, quote
 from user_agents import random_user_agent
@@ -12,59 +14,112 @@ import random
 import jieba
 import execjs
 from langdetect import detect
+from gtoken import TokenAcquirer
+
 
 def gg_fanyi(type, q, dst):
     print("gg_fanyi")
 
-    ctx = execjs.compile("""
-    function TL(a) {
-    var k = "";
-    var b = 406644;
-    var b1 = 3293161072;
+    # with PyV8.JSContext() as ctxt:
+    #     ctxt.eval("""
+    #         function TL(a) {
+    #             var k = "";
+    #             var b = 406644;
+    #             var b1 = 3293161072;
+    #
+    #             var jd = ".";
+    #             var $b = "+-a^+6";
+    #             var Zb = "+-3^+b+-f";
+    #
+    #             for (var e = [], f = 0, g = 0; g < a.length; g++) {
+    #                 var m = a.charCodeAt(g);
+    #                 128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+    #                 e[f++] = m >> 18 | 240,
+    #                 e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
+    #                 e[f++] = m >> 6 & 63 | 128),
+    #                 e[f++] = m & 63 | 128)
+    #             }
+    #             a = b;
+    #             for (f = 0; f < e.length; f++) a += e[f],
+    #             a = RL(a, $b);
+    #             a = RL(a, Zb);
+    #             a ^= b1 || 0;
+    #             0 > a && (a = (a & 2147483647) + 2147483648);
+    #             a %= 1E6;
+    #             return a.toString() + jd + (a ^ b)
+    #         };
+    #
+    #         function RL(a, b) {
+    #             var t = "a";
+    #             var Yb = "+";
+    #             for (var c = 0; c < b.length - 2; c += 3) {
+    #                 var d = b.charAt(c + 2),
+    #                 d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
+    #                 d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
+    #                 a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
+    #             }
+    #             return a
+    #         }
+    #     """)
+    #     print("at here for debug")
+    #     TL = ctxt.locals.TL
+    #     tk = TL(q)
 
-    var jd = ".";
-    var $b = "+-a^+6";
-    var Zb = "+-3^+b+-f";
-
-    for (var e = [], f = 0, g = 0; g < a.length; g++) {
-        var m = a.charCodeAt(g);
-        128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
-        e[f++] = m >> 18 | 240,
-        e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
-        e[f++] = m >> 6 & 63 | 128),
-        e[f++] = m & 63 | 128)
-    }
-    a = b;
-    for (f = 0; f < e.length; f++) a += e[f],
-    a = RL(a, $b);
-    a = RL(a, Zb);
-    a ^= b1 || 0;
-    0 > a && (a = (a & 2147483647) + 2147483648);
-    a %= 1E6;
-    return a.toString() + jd + (a ^ b)
-};
-
-function RL(a, b) {
-    var t = "a";
-    var Yb = "+";
-    for (var c = 0; c < b.length - 2; c += 3) {
-        var d = b.charAt(c + 2),
-        d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
-        d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
-        a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
-    }
-    return a
-}
-""")
+# need call node, start node is slow
+#     ctx = execjs.compile("""
+#     function TL(a) {
+#     var k = "";
+#     var b = 406644;
+#     var b1 = 3293161072;
+#
+#     var jd = ".";
+#     var $b = "+-a^+6";
+#     var Zb = "+-3^+b+-f";
+#
+#     for (var e = [], f = 0, g = 0; g < a.length; g++) {
+#         var m = a.charCodeAt(g);
+#         128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+#         e[f++] = m >> 18 | 240,
+#         e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
+#         e[f++] = m >> 6 & 63 | 128),
+#         e[f++] = m & 63 | 128)
+#     }
+#     a = b;
+#     for (f = 0; f < e.length; f++) a += e[f],
+#     a = RL(a, $b);
+#     a = RL(a, Zb);
+#     a ^= b1 || 0;
+#     0 > a && (a = (a & 2147483647) + 2147483648);
+#     a %= 1E6;
+#     return a.toString() + jd + (a ^ b)
+# };
+#
+# function RL(a, b) {
+#     var t = "a";
+#     var Yb = "+";
+#     for (var c = 0; c < b.length - 2; c += 3) {
+#         var d = b.charAt(c + 2),
+#         d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
+#         d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
+#         a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
+#     }
+#     return a
+# }
+# """)
     # t1 = time.time()
-    tk = ctx.call("TL", q)
+    # tk = ctx.call("TL", q)
     # t2 = time.time()
     # print('took tk ' + str(t2-t1) + ' second')
+
+    session = requests.Session()
+    token_acquirer = TokenAcquirer(session=session, host="translate.google.cn")
+    tk = token_acquirer.do(q)
+    # tk = TTL(q)
     # print(tk)
     # print(q)
-    headers = {'User-Agent':random_user_agent()}
+    headers = {'User-Agent': random_user_agent()}
     # print(q)
-    type = detect(q[:30]) #"en | zh_cn"
+    type = detect(q[:30])  # "en | zh_cn"
     print("detect(q) = " + type)
     # if type == 'ko':
     #     type = "zh-cn"
@@ -163,7 +218,8 @@ function RL(a, b) {
                   "&srcrom=0&ssel=0&tsel=0&kc=2&tk=%s&q=%s" % (tk, q)
 
     # t1 = time.time()
-    r = requests.get(url, headers=headers)
+    # r = requests.get(url, headers=headers)
+    r = session.get(url, headers=headers)
     # t2 = time.time()
     # print('took get ' + str(t2-t1) + ' second')
     # print(r.encoding)
@@ -181,11 +237,12 @@ function RL(a, b) {
 
     return result
 
+
 def youdao_fanyi(type, q, dst):
     print("youdao_fanyi")
     # q = "你好"
     # q = "hello"
-    headers = {'User-Agent':random_user_agent()}
+    headers = {'User-Agent': random_user_agent()}
     # type = detect(q[:30]) #"en | zh_cn"
     # print("detect(q) = " + type)
     url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&sessionFrom=https://www.baidu.com/link'
@@ -222,14 +279,15 @@ def youdao_fanyi(type, q, dst):
 
     return result
 
+
 def xunfei_fanyi(type, q):
     pass
 
 
 def bd_fanyi(type, q, dst):
     print("bd_fanyi")
-    appid = '20190613000307195' #你的appid
-    secretKey = 'GiYXPZ27k7gHPTFFOzke' #你的密钥
+    appid = '20190613000307195'  # 你的appid
+    secretKey = 'GiYXPZ27k7gHPTFFOzke'  # 你的密钥
 
     # httpClient = None
     apiurl = '/api/trans/vip/translate'
@@ -245,28 +303,38 @@ def bd_fanyi(type, q, dst):
 
     salt = random.randint(32768, 65536)
 
-    sign = appid+q+str(salt)+secretKey
+    sign = appid + q + str(salt) + secretKey
     m1 = hashlib.md5()
     m1.update(sign.encode(encoding='utf-8'))
     sign = m1.hexdigest()
     # print(dst)
-    type = detect(q[:30]) #"en | zh_cn"
+    type = detect(q[:30])  # "en | zh_cn"
     print("detect(q) = " + type)
     if dst == "cn":
         # print("dst cn")
         if type == "zh-tw":
-            url = 'http://api.fanyi.baidu.com' + apiurl+'?appid='+appid+'&q='+quote(q)+'&from='+'cht'+'&to='+'zh'+'&salt='+str(salt)+'&sign='+sign
+            url = 'http://api.fanyi.baidu.com' + apiurl + '?appid=' + appid + '&q=' + \
+                quote(q) + '&from=' + 'cht' + '&to=' + 'zh' + \
+                '&salt=' + str(salt) + '&sign=' + sign
         else:
-            url = 'http://api.fanyi.baidu.com' + apiurl+'?appid='+appid+'&q='+quote(q)+'&from='+'auto'+'&to='+'zh'+'&salt='+str(salt)+'&sign='+sign
+            url = 'http://api.fanyi.baidu.com' + apiurl + '?appid=' + appid + '&q=' + \
+                quote(q) + '&from=' + 'auto' + '&to=' + 'zh' + \
+                '&salt=' + str(salt) + '&sign=' + sign
     elif dst == "en":
         # print("dst en")
-        url = 'http://api.fanyi.baidu.com' + apiurl+'?appid='+appid+'&q='+quote(q)+'&from='+'auto'+'&to='+'en'+'&salt='+str(salt)+'&sign='+sign
+        url = 'http://api.fanyi.baidu.com' + apiurl + '?appid=' + appid + '&q=' + \
+            quote(q) + '&from=' + 'auto' + '&to=' + 'en' + \
+            '&salt=' + str(salt) + '&sign=' + sign
     elif dst == "fra":
         # print("dst fr")
-        url = 'http://api.fanyi.baidu.com' + apiurl+'?appid='+appid+'&q='+quote(q)+'&from='+'auto'+'&to='+'fra'+'&salt='+str(salt)+'&sign='+sign
+        url = 'http://api.fanyi.baidu.com' + apiurl + '?appid=' + appid + '&q=' + \
+            quote(q) + '&from=' + 'auto' + '&to=' + 'fra' + \
+            '&salt=' + str(salt) + '&sign=' + sign
     else:
         # print("......")
-        url = 'http://api.fanyi.baidu.com' + apiurl+'?appid='+appid+'&q='+quote(q)+'&from='+'auto'+'&to='+'auto'+'&salt='+str(salt)+'&sign='+sign
+        url = 'http://api.fanyi.baidu.com' + apiurl + '?appid=' + appid + '&q=' + \
+            quote(q) + '&from=' + 'auto' + '&to=' + 'auto' + \
+            '&salt=' + str(salt) + '&sign=' + sign
     # print(url)
     # http://api.fanyi.baidu.com/api/trans/vip/translate?q=apple&from=en&to=zh&appid=2015063000000001&salt=1435660288&sign=f89f9594663708c1605f3d736d01d2d4
     r = requests.get(url)
@@ -276,6 +344,7 @@ def bd_fanyi(type, q, dst):
         result += "\n" + item["dst"]
 
     return result
+
 
 def jieba_cat(type, q):
     # print("jieba_cat")
